29077c35616b7fdc4bd966ffd60589f1338b8a38
heyfidler <heyfidler@gmail.com>
2017-05-10 08:42:43 -0700

    Add files via upload

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,19 @@
+$(document).ready(function() {
+	$("select").change(function() {
+		submitForm();
+	});
+});
 
+function submitForm() {
+	var myData = $("#searchForm").serialize();
+	console.log("data: " + myData);
+	$.ajax({
+		url : "/search",
+		data : myData,
+		dataType : 'html',
+		success : function(data) {
+			$("#collegeData").html(data);
+			console.log("sucess!!: " + data);
+		}
+	});
+}
